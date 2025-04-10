510ac8a2734034f334b57d458c1bdbc222c1d6f8
lujin <lujinmohammed223@gmail.com>
2025-04-10 13:34:18 +0300

    everything so far

--- Saddah/SaddahApp.swift
+++ Saddah/SaddahApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct SaddahApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                SignIn()
+            }
         }
     }
 }
